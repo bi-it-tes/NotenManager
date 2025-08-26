9e0fa34d1a2eb0b648f7d0f25d739becc3283b8f
bi-it-tes <sabina.teleskumar@espas.ch>
2025-08-26 14:45:40 +0200

    Breadcrumbs funktion is implementetn | ToDo: It should depend dynamically

---
+++ Repository/BreadcrumbRepository.cs
@@ -0,0 +1,15 @@
+﻿using NotenManager.Model;
+using System.Collections.Generic;
+
+namespace NotenManager.Repository
+{
+    public class BreadcrumbRepository
+    {
+        public List<BreadcrumbModel> Breadcrumbs { get; private set; } = new List<BreadcrumbModel>();
+
+        public void SetBreadcrumb(List<BreadcrumbModel> breadcrumbs)
+        {
+            Breadcrumbs = breadcrumbs;
+        }
+    }
+}
